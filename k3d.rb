41d6fb1f206f3cd7051fd8b3f961306b2b7610e7
Francois Bayart <francois.bayart@kensu.io>
2020-07-05 13:42:34 +0200

    rename to k3d

--- k3d.rb
+++ k3d.rb
@@ -1,16 +1,20 @@
-class K3d30 < Formula
+class K3d < Formula
   desc "Little helper to run Rancher Lab's k3s v3.0 in Docker"
   homepage "https://github.com/rancher/k3d"
   url "https://github.com/rancher/k3d/archive/v3.0.0-rc.6.tar.gz"
   version "3.0.0-rc6"
   sha256 "75a13b84a45525e653269e25ba9abfe19fec4fac7912ca88c47d690b91ba667a"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "3c1dd20b30c4a51347c2c01a5dc4346e4aa4ffc9162cb9f187df43d333192c28" => :catalina
-    sha256 "0039236f61e40518ebfc684b45a5f13aa3ac9c6da15128b7a67fb020961e1605" => :mojave
-    sha256 "3e63ce46a496a7fe264aa10583115f4fa5e51eaa04020ffbe1c73962fe55b755" => :high_sierra
-  end
+  # bottle do
+  #   cellar :any_skip_relocation
+  #   sha256 "3c1dd20b30c4a51347c2c01a5dc4346e4aa4ffc9162cb9f187df43d333192c28" => :catalina
+  #   sha256 "0039236f61e40518ebfc684b45a5f13aa3ac9c6da15128b7a67fb020961e1605" => :mojave
+  #   sha256 "3e63ce46a496a7fe264aa10583115f4fa5e51eaa04020ffbe1c73962fe55b755" => :high_sierra
+  # end
+
+  # conflicts_with "k3d", :because => "k3d-v3.0 also ships a k3d binary"
+
+  # keg_only "because I want it so"
 
   depends_on "go" => :build
 
